4208743296e70dc24b6d4c2136006f45d3e873b2
Zinno <4070806@vtsd.com>
2020-02-26 14:20:36 -0500

    Initial Upload

---
+++ Military.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public interface Military{
+  public void country();
+  public void information();
+  public void dressCode();
+  public void progress();
+  public void orientation();
+  public void register();
+}
